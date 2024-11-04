c007a881c70db12bd11d6c413a41d0be4696115d
diemquynhtran <quynh.tran@pionero.io>
2024-11-04 14:47:13 +0700

    build 8

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ echo "Installing dependencies..."
 # Stop the current backend service if running
 pm2 stop "test-trk" || true
 
-sudo npm install
 # Navigate to the app directory
 cd /home/ec2-user/cicd
+sudo npm install
 
 # Start the backend service with PM2
 pm2 start dist/main.js --name "test-trk" || pm2 restart "test-trk"
