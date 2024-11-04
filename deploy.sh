2befd47bbb48fc0fc00e1f04fe32a153c49639f5
diemquynhtran <quynh.tran@pionero.io>
2024-11-04 10:58:35 +0700

    add code 8

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,8 @@ pm2 stop backend || true
 
 # Navigate to the app directory
 cd /home/ec2-user/cicd
-
+npm install
+npm run build
 
 # Start the backend service with PM2
 pm2 start dist/main.js --name "backend" || pm2 restart "backend"
